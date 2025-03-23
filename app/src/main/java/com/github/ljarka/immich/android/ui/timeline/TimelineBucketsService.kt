400f933ea937f76be01e6d7c73a4b4e92234ebba
Lukasz Jarka <lukasz.jarka@gmail.com>
2025-03-23 19:20:07 +0100

    Get images with partners

--- app/src/main/java/com/github/ljarka/immich/android/ui/timeline/TimelineBucketsService.kt
+++ app/src/main/java/com/github/ljarka/immich/android/ui/timeline/TimelineBucketsService.kt
@@ -5,13 +5,19 @@ import retrofit2.http.Query
 
 interface TimelineBucketsService {
 
-    @GET("/api/timeline/buckets?size=MONTH")
-    suspend fun getTimeBuckets(): List<TimeBucket>
+    @GET("/api/timeline/buckets")
+    suspend fun getTimeBuckets(
+        @Query("size") size: String = "MONTH",
+        @Query("withPartners") withPartners: Boolean = true,
+        @Query("isArchived") isArchived: Boolean = false,
+    ): List<TimeBucket>
 
     @GET("/api/timeline/bucket")
     suspend fun getBucket(
         @Query("size") size: String,
-        @Query("timeBucket") timeBucket: String
+        @Query("timeBucket") timeBucket: String,
+        @Query("withPartners") withPartners: Boolean = true,
+        @Query("isArchived") isArchived: Boolean = false,
     ): List<Asset>
 
 }
